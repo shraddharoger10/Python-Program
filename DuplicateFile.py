03910cbdf237be0f03ed76e9ad6117e931f4c2ef
shraddharoger10 <86183461+shraddharoger10@users.noreply.github.com>
2022-05-16 11:55:01 +0530

    Add files via upload

---
+++ DuplicateFile.py
@@ -0,0 +1,90 @@
+from sys import *
+import os
+import hashlib
+import time
+
+
+def hashfile(path,blocksize = 1024):
+    afile = open(path,'rb')
+    hasher = hashlib.md5()
+    buf = afile.read(blocksize)
+
+    while len(buf) > 0:
+        hasher.update(buf)
+        buf = afile.read(blocksize)
+    afile.close()
+
+def findDup(path):
+    flag = os.path.isabs(path)
+
+    if flag == False:
+        path = os.path.abspath(path)
+
+    exists = os.path.isdir(path)
+
+    dups = {}
+
+    if exists:
+        for dirName,subdir,fileList in os.walk(path):
+            print("Current folder is : "+dirName)
+            for filen in fileList:
+                path = os.path.join(dirName,filen)
+                file_hash = hashfile(path)
+
+                if file_hash in dups:
+                    dups[file_hash].append(path)
+                else:
+                    dups[file_hash] = [path]
+
+        return dups
+    else:
+        print("Invalid Path")
+
+def printResults(dict1):
+    results = list(filter(lambda x : len(x) > 1,dict1.values()))
+
+    if len(results) > 0:
+        print("Duplicates Found :")
+        print("The following files are duplicate")
+        for result in results:
+            for subresult in result:
+                print("\t\t%s"%subresult)
+    else:
+        print("No duplicate files found.")
+
+
+def main():
+    print("---- Duplicate File Removal by Shraddha-----")
+
+    print("Application name : " + argv[0])
+
+    if (len(argv) != 2):
+        print("Error : Invalid number of arguments")
+        exit()
+
+    if (argv[1] == "-h") or (argv[1] == "-H"):
+        print("This Script is used to traverse specific directory")
+        exit()
+
+    if (argv[1] == "-u") or (argv[1] == "-U"):
+        print("usage : ApplicationName AbsolutePath_of_Directory")
+        exit()
+
+    try:
+        arr = {}
+        startTime = time.time()
+        arr = findDup(argv[1])
+        printResults(arr)
+        endTime = time.time()
+
+        print("Took %s seconds to evaluate."%(endTime-startTime))
+
+    except ValueError:
+        print("Error : Invalid datatype of input")
+
+    except Exception as E:
+        print("Error : Invalid input",E)
+
+
+if __name__ == "__main__":
+    main()
